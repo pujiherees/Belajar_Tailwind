aa6b10be7b6203f44108d8bb447435431c53e95d
pujiherees <susipuji043@gmail.com>
2025-04-08 19:11:49 +0700

    Tugas PIMK

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'accent': '#46A358',
+        'typo': '#3D3D3D',
+        'bg': '#F5F5F5',
+        'desc': '#727272',
+        'white': '#FFFFFF',
+        'light-green': '#46A3581A'
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
